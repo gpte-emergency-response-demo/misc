9dd200ae18801537ea53bd2e2e0a17721f1c51b3
Jeff Bride <jbride@users.noreply.github.com>
2019-08-15 11:00:37 -0500

    Update erdemo_state_refresh.sh

--- erdemo_state_refresh.sh
+++ erdemo_state_refresh.sh
@@ -1,6 +1,7 @@
 rhsso_project=sso
 
 enableLetsEncryptCertsOnRoutes() {
+    oc delete project prod-letsencrypt
     oc new-project prod-letsencrypt
     oc create -fhttps://raw.githubusercontent.com/gpe-mw-training/openshift-acme/master/deploy/letsencrypt-live/cluster-wide/{clusterrole,serviceaccount,imagestream,deployment}.yaml -n prod-letsencrypt
     oc adm policy add-cluster-role-to-user openshift-acme -z openshift-acme -n prod-letsencrypt
